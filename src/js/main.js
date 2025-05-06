100fe0c2637176b3bf4b458428652a5c9be27d45
Charline Rime <160761068+CharlineRime@users.noreply.github.com>
2025-05-06 09:54:13 +0200

    ajout du slider de comparaison

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+var range = document.querySelector(".slider-range");
+
+var imageAfter = document.querySelector(".slider-image-after");
+
+var updatePosition = function () {
+  var position = range.value + "%";
+
+  imageAfter.style.width = position;
+};
+
+range.addEventListener("input", updatePosition);
+
+var range = document.querySelector(".slider-range");
+var imageAfter = document.querySelector(".slider-image-after");
+var separator = document.querySelector(".slider-separator");
+
+var updatePosition = function () {
+  var position = range.value + "%";
+  imageAfter.style.width = position;
+  separator.style.left = position;
+};
+
+range.addEventListener("input", updatePosition);
